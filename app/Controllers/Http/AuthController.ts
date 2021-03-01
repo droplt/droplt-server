231da6c8a70eaf0f144b1412b404afafc12b3266
François Lefebvre <francois.lfe@gmail.com>
2021-03-01 23:40:16 +0100

    chore(project): setup project

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -2,7 +2,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
   public async login({ request, auth }: HttpContextContract) {
-    const [uid, password] = request.only(['uid', 'password'])
+    const { uid, password } = request.only(['uid', 'password'])
     const token = await auth.use().attempt(uid, password)
     return token.toJSON()
   }
--- start/routes.ts
+++ start/routes.ts
@@ -19,8 +19,4 @@
 */
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
-
-Route.resource('users', 'UsersController').apiOnly()
+Route.post('/auth', 'AuthController.login')
